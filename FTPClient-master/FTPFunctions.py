a96469f45852f1fe978f162431fb4d537e891cf1
Mete ALANLI <metealanli@gmail.com>
2016-03-12 23:22:48 +0200

    Last Changes
    We work with our group and make major language changes.

---
+++ FTPClient-master/FTPFunctions.py
@@ -0,0 +1,15 @@
+import sys, os, os.path, operator
+class FTPFunctions:
+    @classmethod
+    def upload(self):
+        return
+    @classmethod
+    def download(self):
+        return
+    @classmethod
+    def getHelp(cls):
+        response =" listele\t'Klasörleri listeler'\n" \
+                  " gulegule\t'Login Ekranina çıkış verir'\n" \
+                  " cikis\t'Programdan çıkmayı sağlar'\n" \
+                  " "
+        return response
