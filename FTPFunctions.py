99980d2629b1e39c3d4a064c2382bdb69598367c
RuyaSibil <ruya.sibil@gmail.com>
2016-03-13 00:26:22 +0200

    Update FTPFunctions.py

--- FTPFunctions.py
+++ FTPFunctions.py
@@ -8,9 +8,13 @@ class FTPFunctions:
         return
     @classmethod
     def getHelp(cls):
-        response =" listele\t'Klasörleri listeler'\n" \
+        response = "listele\t'Klasörleri listeler'\n" \
                   " gulegule\t'Login Ekranina çıkış verir'\n" \
                   " cikis\t'Programdan çıkmayı sağlar'\n" \
-                  " k_gir\t'Belirtilen yola gir\n'" \
-                  " "
+                  " k_gir\t'Belirtilen yola girin\n'" \
+                  " k_duzenle\t'Belirtilen yolu duzenle\n'" \
+                  " k_kopyala\t'Belirtilen yolu kopyala\n'" \
+                  " k_yapistir\t'Belirtilen yolu yapistirir\n'" \
+                  " k_sil\t'Belirtilen yolu siler\n'" \
+                  " k_zip\t'Belirtilen zip.li yolu acma\n'" \
         return response
